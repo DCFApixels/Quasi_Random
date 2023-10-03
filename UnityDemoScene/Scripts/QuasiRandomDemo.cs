018c6f61a0c208e424a1902386c9ddc4419048a4
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2023-10-03 21:23:53 +0800

    update unity demo scene

--- UnityDemoScene/Scripts/QuasiRandomDemo.cs
+++ UnityDemoScene/Scripts/QuasiRandomDemo.cs
@@ -7,18 +7,29 @@ public class QuasiRandomDemo : MonoBehaviour
     public int count = 100;
     public Gradient gradient;
     public float size;
+    public int seed;
+    public bool autoSeed = true;
+
+    private QuasiRandom _random;
+
+    private void Awake()
+    {
+        if (autoSeed)
+            _random = QuasiRandom.AutoSeed();
+        else
+            _random = new QuasiRandom(seed);
+    }
 
     private void OnEnable()
     {
-        QuasiRandom random = QuasiRandom.AutoSeed();
-
         Transform transform = this.transform;
         Vector2 halfSize = Vector2.one * size / 2f;
         for (int i = 0; i < count; i++)
         {
             SpriteRenderer point = Instantiate(pointPrefab, transform);
-            point.transform.localPosition = random.NextUnityVector2() * size - halfSize;
+            point.transform.localPosition = _random.NextUnityVector2() * size - halfSize;
             point.color = gradient.Evaluate((float)i / count);
+            point.sortingOrder = i;
         }
     }
 }
--- UnityDemoScene/Scripts/UnityRandomDemo.cs
+++ UnityDemoScene/Scripts/UnityRandomDemo.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DefaultRandom = System.Random;
 
 public class UnityRandomDemo : MonoBehaviour
 {
@@ -6,16 +7,29 @@ public class UnityRandomDemo : MonoBehaviour
     public int count = 100;
     public Gradient gradient;
     public float size;
+    public int seed;
+    public bool autoSeed = true;
+
+    private DefaultRandom _random;
+
+    private void Awake()
+    {
+        if (autoSeed)
+            _random = new DefaultRandom();
+        else
+            _random = new DefaultRandom(seed);
+    }
 
     private void OnEnable()
     {
         Transform transform = this.transform;
-        float halfSize = size / 2f;
+        Vector2 halfSize = Vector2.one * size / 2f;
         for (int i = 0; i < count; i++)
         {
             SpriteRenderer point = Instantiate(pointPrefab, transform);
-            point.transform.localPosition = new Vector3(Random.value * size - halfSize, Random.value * size - halfSize, 0);
+            point.transform.localPosition = new Vector2((float)_random.NextDouble(), (float)_random.NextDouble()) * size - halfSize;
             point.color = gradient.Evaluate((float)i / count);
+            point.sortingOrder = i;
         }
     }
 }
